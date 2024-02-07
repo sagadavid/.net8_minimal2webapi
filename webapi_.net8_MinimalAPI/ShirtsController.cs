ccdb955e084fd4ef5fc3725f83a60bac69c68181
sagadavid <epostsaga@gmail.com>
2024-02-07 14:31:08 +0100

    controller routing v2

--- webapi_.net8_MinimalAPI/ShirtsController.cs
+++ webapi_.net8_MinimalAPI/ShirtsController.cs
@@ -4,41 +4,42 @@ using Microsoft.AspNetCore.Mvc;
 namespace webapi_.net8_MinimalAPI
 {
     [ApiController]
+    [Route("[Controller]")]
     public class ShirtsController : ControllerBase
     {
         [HttpGet]
-        [Route("/shirts")]
+        //[Route("/shirts")]
         public string GetShirts() {
             return "getting from controller";
         }
 
-        [HttpGet]
-        [Route("/shirts/{id}")]
+        [HttpGet("{id}")]
+        //[Route("/shirts/{id}")]
         public string GetShirtById(int id)
         {
             return $"getting shirt id {id} from controller";
         }
 
         [HttpPost]
-        [Route("/shirts")]
+        //[Route("/shirts")]
         public string CreateShirt()
         {
             return $"creating shirts from controller";
         }
 
-        [HttpPut]
-        [Route("/shirts/{id}")]
+        [HttpPut("{id}")]
+        //[Route("/shirts/{id}")]
         public string UpdateShirts(int id)
         {
             return $"updating shirt id {id} from controller";
 
         }
 
-        [HttpDelete]
-        [Route("/shirts/{id}")]
+        [HttpDelete("{id}")]
+        //[Route("/shirts/{id}")]
         public string DeleteShirt(int id)
         {
-            return $"deleting from controller by id {id}";
+            return $"deleting shirt {id} from controller";
         }
         
     }
