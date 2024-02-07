06e75f7b93030021bc43242ced7a411324d51ac6
sagadavid <epostsaga@gmail.com>
2024-02-07 14:26:12 +0100

    controller based routing to endpoints

--- webapi_.net8_MinimalAPI/Program.cs
+++ webapi_.net8_MinimalAPI/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -10,31 +11,7 @@ app.UseHttpsRedirection();
 
 //Routing
 
-app.MapGet("/shirts", () =>
-{
-    return "getting all shirts";
-});
-
-app.MapGet("/shirts/{id}", (int id) =>
-{
-    return $"getting shirt id: {id}";
-});
-
-app.MapPost("/shirts", () =>
-{
-    return "creating-posting all shirts";
-});
-
-
-app.MapPut("/shirts/{id}", (int id) =>
-{
-    return $"putting-updating shirt id: {id}";
-});
-
-app.MapDelete("/shirts/{id}", (int id) =>
-{
-    return $"deleting shirt id: {id}";
-});
+app.MapControllers();
 
 
 app.Run();
---
+++ webapi_.net8_MinimalAPI/ShirtsController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi_.net8_MinimalAPI
+{
+    [ApiController]
+    public class ShirtsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/shirts")]
+        public string GetShirts() {
+            return "getting from controller";
+        }
+
+        [HttpGet]
+        [Route("/shirts/{id}")]
+        public string GetShirtById(int id)
+        {
+            return $"getting shirt id {id} from controller";
+        }
+
+        [HttpPost]
+        [Route("/shirts")]
+        public string CreateShirt()
+        {
+            return $"creating shirts from controller";
+        }
+
+        [HttpPut]
+        [Route("/shirts/{id}")]
+        public string UpdateShirts(int id)
+        {
+            return $"updating shirt id {id} from controller";
+
+        }
+
+        [HttpDelete]
+        [Route("/shirts/{id}")]
+        public string DeleteShirt(int id)
+        {
+            return $"deleting from controller by id {id}";
+        }
+        
+    }
+}
