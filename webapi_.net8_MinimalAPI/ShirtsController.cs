83c6430288633d11e93a821776e903ce31ab7758
sagadavid <epostsaga@gmail.com>
2024-02-07 20:45:20 +0100

    return type as model

--- webapi_.net8_MinimalAPI/ShirtsController.cs
+++ webapi_.net8_MinimalAPI/ShirtsController.cs
@@ -7,28 +7,34 @@ namespace webapi_.net8_MinimalAPI
     [Route("api/[Controller]")]
     public class ShirtsController : ControllerBase
     {
+
+        private List<ShirtModel> shirts = new List<ShirtModel>()
+        {
+        new ShirtModel { Id = 1, Brand = "Matsumi", Color="Greenish", Gender="Female", Price=21, Size=5},
+         new ShirtModel { Id = 2, Brand = "Takusaki", Color="Purple", Gender="Male", Price=21, Size=6},
+          new ShirtModel { Id = 3, Brand = "Finsuta", Color="Blue", Gender="Male", Price=21, Size=9},
+           new ShirtModel { Id = 4, Brand = "Komansi", Color="Yellowish", Gender="Female", Price=21, Size=4}
+        };
+
         [HttpGet]
-        //[Route("/shirts")]
         public string GetAllShirts() {
             return "getting from controller";
         }
 
         [HttpGet("{id}")]
-        //[Route("/shirts/{id}")]
-        public string GetShirtBy(int id)
+        public ShirtModel GetShirtBy(int id)
         {
-            return $"getting shirt id {id} from controller";
+            return shirts.First(x=>x.Id == id);
+            
         }
 
         [HttpPost]
-        //[Route("/shirts")]
         public string CreateShirt([FromForm]ShirtModel shirt)
         {
             return $"creating shirts from controller";
         }
 
         [HttpPut("{id}")]
-        //[Route("/shirts/{id}")]
         public string UpdateShirtBy(int id)
         {
             return $"updating shirt id {id} from controller";
@@ -36,7 +42,6 @@ namespace webapi_.net8_MinimalAPI
         }
 
         [HttpDelete("{id}")]
-        //[Route("/shirts/{id}")]
         public string DeleteShirtBy(int id)
         {
             return $"deleting shirt {id} from controller";
