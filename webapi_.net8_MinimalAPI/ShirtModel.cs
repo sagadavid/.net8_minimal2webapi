6bd32c1d186ec4f17fe3b74472092d34343dd6ab
sagadavid <epostsaga@gmail.com>
2024-02-07 19:18:51 +0100

    Validation via data annotation

--- webapi_.net8_MinimalAPI/ShirtModel.cs
+++ webapi_.net8_MinimalAPI/ShirtModel.cs
@@ -1,11 +1,15 @@
-﻿namespace webapi_.net8_MinimalAPI
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace webapi_.net8_MinimalAPI
 {
     public class ShirtModel
     {
+     
         public int Id { get; set; }
         public string? Brand { get; set; }
         public string? Color { get; set; }
         public string? Gender { get; set; }
+        [Required]
         public string? Size { get; set; }    
         public double Price { get; set; }
 
