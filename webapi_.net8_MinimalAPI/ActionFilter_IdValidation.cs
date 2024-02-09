5269e41fb6a481e733bd4222fe8b325a2bd19124
sagadavid <epostsaga@gmail.com>
2024-02-09 20:39:35 +0100

    standard error messages via ValidationProblemDetails

--- webapi_.net8_MinimalAPI/ActionFilter_IdValidation.cs
+++ webapi_.net8_MinimalAPI/ActionFilter_IdValidation.cs
@@ -15,13 +15,21 @@ namespace webapi_.net8_MinimalAPI
                 if (idInAction.Value <= 0)
                 {
                     context.ModelState.AddModelError("id", "shirt id value is invalid");
-                    context.Result = new BadRequestObjectResult(context.ModelState);
+                    var problemDetails = new ValidationProblemDetails(context.ModelState)
+                    {
+                        Status = StatusCodes.Status400BadRequest
+                    };
+                    context.Result = new BadRequestObjectResult(problemDetails);
                 }
 
                 else if (!Repository.shirtExists(idInAction.Value))
                 {
                     context.ModelState.AddModelError("id", "shirt doesnt exist");
-                    context.Result = new NotFoundObjectResult(context.ModelState);
+                    var problemDetails = new ValidationProblemDetails(context.ModelState)
+                    {
+                        Status = StatusCodes.Status404NotFound
+                    };
+                    context.Result = new NotFoundObjectResult(problemDetails);
 
                 }
             }
